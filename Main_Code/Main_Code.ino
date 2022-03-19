22aa7f90e524cdcdba271d811301d29ce6d96631
komara6894 <komara6894@sdsu.edu>
2022-03-19 00:30:50 -0700

    New Function running off serial port.

--- Main_Code/Main_Code.ino
+++ Main_Code/Main_Code.ino
@@ -69,107 +69,299 @@ int ESD_Ground = 39;                        //Tower Ground fail.
 //--------------------------------------------------------------------------------------
 boolean Enable = LOW;                       //Defualt State for Enable Pin is High to make the Motor Disabled by default.
 boolean Reverse = LOW;                      //Defualt State for Direction Pin is Low to make the Motor Direction CW by defauult.
-int Pulse_Delay = 500;                      //Pulse Delay Period.
+int Pulse_Delay = 2500;                      //Pulse Delay Period.
+int Slave = 0;
+String Mode;
+int Trays_Count = 0;
+static bool runOnlyOnce = false;
+static bool runOnlyOnceL1 = false;
 
 
 void setup()
 {
-  Serial.begin(115200);                    //Initiating Remote serial communication for Main System.
-  //Serial1.begin(115200);                   /Initiating serial communication for LCD.
-  //Serial2.begin(115200);                   //Initiating serial communication for Slave System.  
+  Serial.begin(9600);                    //Initiating Remote serial communication for Main System.
+  //Serial1.begin(9600);                   /Initiating serial communication for LCD.
+  //Serial2.begin(9600);                   //Initiating serial communication for Slave System.
   pinMode (M_Manual, INPUT);                 //Defining Pins Mode (Input).
   pinMode (M_Speed, INPUT);                  //Defining Pins Mode (Input).
+  digitalWrite(M_Speed, LOW);
   pinMode (M_Enable, INPUT);                 //Defining Pins Mode (Input).
   pinMode (M_Reverse, INPUT);                //Defining Pins Mode (Input).
   pinMode (M_Up, INPUT);                     //Defining Pins Mode (Input).
   pinMode (M_Down, INPUT);                   //Defining Pins Mode (Input).
   pinMode (M_Enabled, OUTPUT);               //Defining Pins Mode (Output).
   pinMode (M_Manual_Override, OUTPUT);       //Defining Pins Mode (Output).
+  digitalWrite(M_Manual_Override, LOW);
   pinMode (M_Dir, OUTPUT);                   //Defining Pins Mode (Output).
   pinMode (M_Pul, OUTPUT);                   //Defining Pins Mode (Output).
   pinMode (M_En, OUTPUT);                    //Defining Pins Mode (Output).
+  digitalWrite(M_En, LOW);
   pinMode (M_CW, OUTPUT);                    //Defining Pins Mode (Output).
+  digitalWrite(M_CW, LOW);
   pinMode (M_CCW, OUTPUT);                   //Defining Pins Mode (Output).
+  digitalWrite(M_CCW, LOW);
   pinMode (M_Driver_Activate, OUTPUT);       //Defining Pins Mode (Output).
   pinMode (S_Rd, OUTPUT);                    //Defining Pins Mode (Output).
   pinMode (S_Og, OUTPUT);                    //Defining Pins Mode (Output).
   pinMode (S_Gn, OUTPUT);                    //Defining Pins Mode (Output).
   pinMode (S_Buzz, OUTPUT);                  //Defining Pins Mode (Output).
-  pinMode (L_Top_Shift, INPUT);              //Defining Pins Mode (Input).
-  pinMode (L_Tray_Shift, INPUT);             //Defining Pins Mode (Input).
-  pinMode (L_SizeA_Shift, INPUT);            //Defining Pins Mode (Input).
-  pinMode (L_SizeB_Shift, INPUT);            //Defining Pins Mode (Input).
-  pinMode (B_Emergency, INPUT);              //Defining Pins Mode (Input).
-  pinMode (B_RangeUp, INPUT);                //Defining Pins Mode (Input).
-  pinMode (B_RangeDown, INPUT);              //Defining Pins Mode (Input).
+  pinMode (L_Top_Shift, INPUT_PULLUP);              //Defining Pins Mode (Input).
+  pinMode (L_Tray_Shift, INPUT_PULLUP);             //Defining Pins Mode (Input).
+  pinMode (L_SizeA_Shift, INPUT_PULLUP);            //Defining Pins Mode (Input).
+  pinMode (L_SizeB_Shift, INPUT_PULLUP);            //Defining Pins Mode (Input).
+  pinMode (B_Emergency, INPUT_PULLUP);              //Defining Pins Mode (Input).
+  pinMode (B_RangeUp, INPUT_PULLUP);                //Defining Pins Mode (Input).
+  pinMode (B_RangeDown, INPUT_PULLUP);              //Defining Pins Mode (Input).
   pinMode (Slave_Detect, INPUT);             //Defining Pins Mode (Input).
   pinMode (ESD_Ground, INPUT);               //Defining Pins Mode (Input).
-  
+  delay(500);
+  Serial.println("MAS-100 Stacker & Destacker");
+  delay(100);
+  Serial.println("initializing....");
+  initializing();
+  delay(100);
+
   //--------------------------------------------------------------------------------------
   //Interrupt Functions.
   //--------------------------------------------------------------------------------------
-  attachInterrupt(digitalPinToInterrupt(M_Manual_Override), Manual_Override, FALLING);         //Interrupt to Manual Override when Switch pressed.
-  attachInterrupt(digitalPinToInterrupt(M_Enable), Enable_Motor, FALLING);                     //Interrupt to Enable Motor when Switch pressed.
-  attachInterrupt(digitalPinToInterrupt(M_Reverse), Reverse_Motor, FALLING);                   //Interrupt to Reverse Motor when Switch pressed.
-  attachInterrupt(digitalPinToInterrupt(M_Up), Move_Up, FALLING);                              //Interrupt to Move Motor Up when Switch pressed.
-  attachInterrupt(digitalPinToInterrupt(M_Down), Move_Down, FALLING);                          //Interrupt to Move Motor Down when Switch pressed.
+//attachInterrupt(digitalPinToInterrupt(M_Manual_Override), Manual_Override, FALLING);         //Interrupt to Manual Override when Switch pressed.
+}
 
-  //--------------------------------------------------------------------------------------
-  //Default Values for LED Indicators.
-  //--------------------------------------------------------------------------------------
-  digitalWrite(M_En, Enable);                 //Motor Enable LED to Follow M_Enable Variable Logic (Activate Low).
-  digitalWrite(M_CW, Reverse);                //Motor CW LED to Follow Reverse.
-  digitalWrite(M_CCW, !Reverse);              //Motor CCW LED to Follow Oposite of Reverse.
-  digitalWrite(S_Rd, HIGH);                   //Default Status Light LED Is High (Off).
-  digitalWrite(S_Og, HIGH);                   //Default Status Light LED Is High (Off).
-  digitalWrite(S_Gn, HIGH);                   //Default Status Light LED Is High (Off).
-  digitalWrite(S_Buzz, HIGH);                 //Default Status Light Buzzer Is High (Off).
 
-}  
-void loop()
-{ 
-  Pulse_Delay = map((analogRead(M_Speed)),0,1023,2500,35);
-  digitalWrite(M_Dir, Reverse);
-  digitalWrite(M_En, !Enable);
-  digitalWrite(M_CW,Reverse);
-  digitalWrite(M_CCW,!Reverse);
-  digitalWrite(M_Pul, HIGH);
-  delayMicroseconds(Pulse_Delay);
-  digitalWrite(M_Pul, LOW);
+void loop() 
+{
+   //if (runOnlyOnceL1 = false)
+   //{
+    Level1();
+   //}
+//Idle();
+//delay(100);
 }
 
 //--------------------------------------------------------------------------------------
 //Code Functions.
 //--------------------------------------------------------------------------------------
+void initializing()
+{
+  digitalWrite(S_Rd, LOW);                   //Default Status Light LED Is High (Off).
+  delay(1000);
+  digitalWrite(S_Og, LOW);                   //Default Status Light LED Is High (Off).
+  delay(1000);
+  digitalWrite(S_Gn, LOW);                   //Default Status Light LED Is High (Off).
+  delay(1000);
+  digitalWrite(S_Buzz, LOW);                 //Default Status Light Buzzer Is High (Off).
+  delay(1000);
+  digitalWrite(S_Rd, HIGH);                   //Default Status Light LED Is High (Off).
+  digitalWrite(S_Rd, HIGH);                   //Default Status Light LED Is High (Off).
+  digitalWrite(S_Rd, HIGH);                   //Default Status Light LED Is High (Off).
+  digitalWrite(S_Rd, HIGH);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_Driver_Activate, HIGH);
+  delay(400);
+  digitalWrite(M_Driver_Activate, LOW);
+  digitalWrite(Slave_Detect, LOW);
+  Slave = analogRead(Slave_Detect);
+  digitalWrite(M_Manual_Override, LOW);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_Enabled, LOW);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_CW, LOW);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_CCW, LOW);                   //Default Status Light LED Is High (Off).
+  delay(1000);
+  digitalWrite(M_Manual_Override, HIGH);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_Enabled, HIGH);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_CW, HIGH);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_CCW, HIGH);
+  Serial.println("Machine Initialized Successfully......");
+  Serial.println("Type Command (stacker, destacker, manual) to start operation....");
+  delay(200);
+}
+
+void Level1()
+{
+  if (Serial.available())
+  {
+    Mode = Serial.readStringUntil('\n');
+    Mode.trim();
+    if (Mode.equals("stacker"))
+    {
+      Serial.print("Mode: ");
+      Serial.println(Mode);
+      //runOnlyOnceL1 = true;
+      Stacker();
+    }
+
+    else if (Mode.equals("destacker"))
+    {
+     Serial.print("Mode: ");
+     Serial.println(Mode);
+     //runOnlyOnceL1 = true;
+     Destacker(); 
+    }
+
+    else if (Mode.equals("manual"))
+    {
+     Serial.print("Mode: ");
+     Serial.println(Mode);
+     //runOnlyOnceL1 = true; 
+     Manual_Override();
+    }
+
+    else
+    {
+      Serial.println("Bad Command, Try again...");
+    }
+    Serial.print("Mode: ");
+    Serial.println(Mode);
+   // runOnlyOnceL1 = true;
+  }
+//Level1();
+}
+void Stacker()
+{
+  Stacker_Preset();
+
+}
+void Destacker()
+{
+
+}
 void Manual_Override()
 {
+  digitalWrite(M_Manual_Override, LOW);  
+  Pulse_Delay = map((analogRead(M_Speed)),0,1023,2500,300);
+  //delay(1000);
+  Serial.print("\n");
+  Serial.print(Pulse_Delay);
+  if (digitalRead(M_Enable) == LOW)
+  {
+    Enable = !Enable;                         //Enabling or disabling the Motor By programming Oposite logic.
+    digitalWrite(M_Driver_Activate, Enable);
+    digitalWrite(M_Enabled, !Enable);                 //Motor Enable LED to Follow M_Enable Variable Logic (Activate Low)
+    delay(100);
+  }
+ else if(digitalRead(M_Reverse) == LOW)
+ {
+  Reverse = !Reverse;
+  digitalWrite(M_Dir, Reverse);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_CW, Reverse);                //Motor CW LED to Follow Reverse.
+  digitalWrite(M_CCW, !Reverse);              //Motor CCW LED to Follow Oposite of Reverse
+  delay(200);
+ }
+while(digitalRead(M_Up) == LOW)
+ {
+  digitalWrite(M_Dir, HIGH);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_CW, LOW);                //Motor CW LED to Follow Reverse.
+  digitalWrite(M_CCW, HIGH);              //Motor CCW LED to Follow Oposite of Reverse
+  digitalWrite(M_Pul, HIGH);
+  delayMicroseconds(Pulse_Delay);
+  digitalWrite(M_Pul, LOW);
+  delayMicroseconds(Pulse_Delay);
+ }
+while(analogRead(M_Down) == LOW)
+ {
+  digitalWrite(M_Dir, LOW);                   //Default Status Light LED Is High (Off).
+  digitalWrite(M_CW, HIGH);                //Motor CW LED to Follow Reverse.
+  digitalWrite(M_CCW, LOW);              //Motor CCW LED to Follow Oposite of Reverse
+  digitalWrite(M_Pul, HIGH);
+  delayMicroseconds(Pulse_Delay);
+  digitalWrite(M_Pul, LOW);
+  delayMicroseconds(Pulse_Delay);
+ }
+ delay(200);
+ Manual_Override();
+}
+
+
+void Stacker_Preset()
+{
+  while ((digitalRead(L_SizeA_Shift) == HIGH) && ((digitalRead(B_RangeDown) == HIGH)))
+    {
+    digitalWrite(M_Driver_Activate, HIGH);
+    digitalWrite(M_Enabled, LOW);
+    digitalWrite(M_Dir, HIGH);
+    digitalWrite(M_CW, LOW);
+    digitalWrite(M_CCW, HIGH);
+    for (int x = 0; x < 2430; x++)
+    {
+      digitalWrite(M_Pul, HIGH);
+      delayMicroseconds(Pulse_Delay);
+      digitalWrite(M_Pul, LOW);
+      delayMicroseconds(Pulse_Delay);
+      Pulse_Delay--;
+    }
+    digitalWrite(M_Pul, HIGH);
+    delayMicroseconds(Pulse_Delay);
+    digitalWrite(M_Pul, LOW);
+    delayMicroseconds(Pulse_Delay);
+    }
   
+  if((digitalRead(L_SizeA_Shift) == LOW) && ((digitalRead(B_RangeDown) == HIGH)))
+    {
+      Stacker_Preset_S();
+    }
 }
 
-void Enable_Motor()
+void Stacker_Preset_S()
 {
-  Enable = !Enable;                         //Enabking or disabling the Motor By programming Oposite logic.
+  Pulse_Delay = 2500;
+  while (digitalRead(B_RangeDown) == HIGH)
+  {
+    digitalWrite(M_Pul, HIGH);
+    delayMicroseconds(Pulse_Delay);
+    digitalWrite(M_Pul, LOW);
+    delayMicroseconds(Pulse_Delay);
+  }
+  if (digitalRead(B_RangeDown) == LOW)
+  {
+    digitalWrite(M_Dir, LOW);
+    digitalWrite(M_CW, HIGH);
+    digitalWrite(M_CCW, LOW);
+    for (int y = 0; y < 300; y++)
+    {
+      digitalWrite(M_Pul, HIGH);
+      delayMicroseconds(Pulse_Delay);
+      digitalWrite(M_Pul, LOW);
+      delayMicroseconds(Pulse_Delay);
+    }
+   Next_Trigger();
+  }
 }
-
-void Reverse_Motor()
+  void Next_Trigger()
+  {
+    while((digitalRead(L_Top_Shift) == HIGH) && (digitalRead(B_RangeDown) == HIGH))
+    {
+      digitalWrite(M_Pul, HIGH);
+      delayMicroseconds(Pulse_Delay);
+      digitalWrite(M_Pul, LOW);
+      delayMicroseconds(Pulse_Delay);
+    }
+   
+    while((digitalRead(L_Top_Shift) == LOW) && (digitalRead(B_RangeDown) == HIGH))
+    {
+       Idle();
+    }  
+    if (digitalRead(B_RangeDown) == LOW)
+    {
+      Mode_End();
+    }
+  }
+  
+ void Mode_End()
+ {
+  
+ }
+void Idle()
 {
-  Reverse = !Reverse;                       //Revesing the Motor Direction By programming Oposite logic.
+   if (runOnlyOnce == false) 
+   {
+      Trays_Count++;
+      runOnlyOnce = true;
+   }
+ Serial.println("Processed Trays Count =");
+ Serial.print(Trays_Count);
+ delay(1000);
 }
-
-void Move_Up()
-{
-  boolean Reverse = LOW;                      
-  digitalWrite(M_Dir, Reverse);
-  digitalWrite(M_Pul, HIGH);
-  delayMicroseconds(Pulse_Delay);
-  digitalWrite(M_Pul, LOW);
-}
-
-void Move_Down()
-{ 
-  boolean Reverse = LOW;                      
-  digitalWrite(M_Dir, Reverse);
-  digitalWrite(M_Pul, HIGH);
-  delayMicroseconds(Pulse_Delay);
-  digitalWrite(M_Pul, LOW);
-}
+ void Trays_Processed()
+ {
+      Trays_Count++;
+      Serial.println("Processed Trays Count =");
+      Serial.print(Trays_Count);
+ }
